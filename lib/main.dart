8a08e20e603ace90f49759d0cdb83bef128fb390
IkhsanStwn <81740263+IkhsanStwn@users.noreply.github.com>
2022-03-17 10:24:39 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Kuis TPM 2022',
       theme: ThemeData(),
-      home: LoginPage(),
+      home: ListPage(),
     );
   }
 }
